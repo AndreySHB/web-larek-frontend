b5cfcb4e5042485e5c33c1640f9d3bde7ad2d84f
SHubnyiAG <SHubnyiAG@kadastr.ru>
2025-07-22 16:55:57 +0300

    рефакторинг Card.ts

--- src/components/Card.ts
+++ src/components/Card.ts
@@ -13,21 +13,22 @@ export interface ICard {
     description?: string;
 }
 
-export class CatalogCard extends View<ICard> {
+class BaseCard extends View<ICard> {
     protected _title: HTMLElement;
     protected _image: HTMLImageElement;
     protected _category: HTMLElement;
     protected _price: HTMLElement;
     protected _button: HTMLButtonElement;
 
-    constructor(container: HTMLElement, actions?: ICardActions) {
+    constructor(container: HTMLElement) {
         super(container);
         this._title = ensureElement<HTMLElement>('.card__title', container);
         this._image = ensureElement<HTMLImageElement>('.card__image', container);
         this._category = ensureElement<HTMLElement>('.card__category', container);
         this._price = ensureElement<HTMLElement>('.card__price', container);
-        this._button = container.querySelector('.gallery__item');
+    }
 
+    protected setActions(container: HTMLElement, actions?: ICardActions) {
         if (actions?.onClick) {
             if (this._button) {
                 this._button.addEventListener('click', actions.onClick);
@@ -66,38 +67,22 @@ export class CatalogCard extends View<ICard> {
     }
 }
 
-export class PreviewCard extends View<ICard> {
-    protected _title: HTMLElement;
+export class CatalogCard extends BaseCard {
+    constructor(container: HTMLElement, actions?: ICardActions) {
+        super(container);
+        this._button = container.querySelector('.gallery__item');
+        super.setActions(container, actions);
+    }
+}
+
+export class PreviewCard extends BaseCard {
     private _description: HTMLElement;
-    protected _image: HTMLImageElement;
-    protected _category: HTMLElement;
-    protected _price: HTMLElement;
-    protected _button: HTMLButtonElement;
 
     constructor(container: HTMLElement, actions?: ICardActions) {
         super(container);
-        this._title = ensureElement<HTMLElement>('.card__title', container);
         this._description = ensureElement<HTMLElement>('.card__text', container);
-        this._image = ensureElement<HTMLImageElement>('.card__image', container);
-        this._category = ensureElement<HTMLElement>('.card__category', container);
-        this._price = ensureElement<HTMLElement>('.card__price', container);
         this._button = container.querySelector('.card__button');
-
-        if (actions?.onClick) {
-            if (this._button) {
-                this._button.addEventListener('click', actions.onClick);
-            } else {
-                container.addEventListener('click', actions.onClick);
-            }
-        }
-    }
-
-    set title(value: string) {
-        this.setText(this._title, value);
-    }
-
-    get title(): string {
-        return this._title.textContent || '';
+        super.setActions(container, actions);
     }
 
     set description(value: string) {
@@ -106,25 +91,5 @@ export class PreviewCard extends View<ICard> {
 
     get description(): string {
         return this._description.textContent || '';
-    }
-
-    set price(value: number) {
-        this.setText(this._price, value + ' синапсов');
-    }
-
-    get price(): number {
-        return this._price.textContent as unknown as number || 0;
-    }
-
-    set category(value: string) {
-        this.setText(this._category, value);
-    }
-
-    get category(): string {
-        return this._category.textContent || '';
-    }
-
-    set image(value: string) {
-        this.setImage(this._image, value, this.title);
     }
 }
